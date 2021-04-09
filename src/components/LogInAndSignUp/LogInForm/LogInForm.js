c0c32c9bfa517b655ca0d2308fea3a194c079ec0
yichenfromhyrule <jambako1995@gmail.com>
2021-04-09 11:52:52 -0400

    login form customize

--- src/components/LogInAndSignUp/LogInForm/LogInForm.js
+++ src/components/LogInAndSignUp/LogInForm/LogInForm.js
@@ -5,27 +5,40 @@ import './LogInForm.css';
 class LogInForm extends React.Component {
     constructor(props){
         super(props);
-        this.state = {userEmail: ''};
+        this.state = {
+            userEmail: '',
+            userPassword: ''
+        };
     }
     mySubmitHandler = (event) => {
-        alert("You are submitting"+this.state.userEmail);
+        alert("You are submitting"+this.state.userEmail+" password: " + this.state.userPassword);
     }
     myEmailHandler = (event) =>{
         this.setState({userEmail: event.target.value});
     }
+    myPasswordHandler = (event) =>{
+        this.setState({userPassword: event.target.value});
+    }
     render() {
         return (
             <div className="logFormContainer">
-                <form onSubmit={this.mySubmitHandler}>
-                    <label>
-                        Email:
-                        <input 
-                            type="text"
-                            name="name"
-                            onChange={this.myEmailHandler}
-                        />
-                    </label>
+                <form className="logFormForm" onSubmit={this.mySubmitHandler}>
+                    <input 
+                        className="logFormInput"
+                        type="text"
+                        name="email"
+                        placeholder="Enter Email"
+                        onChange={this.myEmailHandler}
+                    />
+                    <input 
+                        className="logFormInput"
+                        type="text"
+                        name="password"
+                        placeholder="Enter Password"
+                        onChange={this.myPasswordHandler}
+                    />
                     <input
+                        className="logFormButton"
                         type="submit"
                         value="Submit"
                     />
