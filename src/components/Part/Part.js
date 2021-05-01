42ba49daafe68a39b8e25c522acb7ea7dda23c9b
yichenfromhyrule <jambako1995@gmail.com>
2021-05-01 11:54:59 -0400

    HomePage Layout

--- src/components/Part/Part.js
+++ src/components/Part/Part.js
@@ -6,19 +6,36 @@ function Part() {
         <div className="partContainer_level1">
             <div className="partContainer_level2_a">
                 <div className="partContainer_level3_a">
-                    <div className="partContainer_Comments">
+                    <div className="partContainer_level4_a">
+
+                    </div>
+                </div>
+                <div className="partContainer_level3_b">
+                    <div className="partContainer_level4_b">
+                        <h1>To Do List</h1>
+                    </div>
+                </div>
+                <div className="partContainer_level3_c">
+                    <div className="partContainer_level4_a">
 
                     </div>
                 </div>
             </div>
             <div className="partContainer_level2_b">
-                <div className="partContainer_Comments">
+                <div className="partContainer_level4_a">
                         
                 </div>
             </div>
             <div className="partContainer_level2_c">
-                <div className="partContainer_Comments">
-                        
+                <div className="partContainer_level3_d">
+                    <div className="partContainer_level4_a">
+
+                    </div>
+                </div>
+                <div className="partContainer_level3_e">
+                    <div className="partContainer_level4_a">
+
+                    </div>
                 </div>
             </div>
         </div>
