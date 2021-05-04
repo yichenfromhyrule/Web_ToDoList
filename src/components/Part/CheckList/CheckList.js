f50ebf59df4ccbca5a546ef1c433a82adeba9875
yichenfromhyrule <jambako1995@gmail.com>
2021-05-03 21:27:34 -0400

    edit

--- src/components/Part/CheckList/CheckList.js
+++ src/components/Part/CheckList/CheckList.js
@@ -4,7 +4,10 @@ import "./CheckList.css";
 function CheckList() {
     return (
         <div className="checkList_Container">
-            <button className="checkList_Edit_Button">+</button>
+            <div className="checkList_Check_Box_Container"></div>
+            <div className="checkList_Button_Container">
+                <button className="checkList_Edit_Button">编辑</button>
+            </div>
             
         </div>
     )
