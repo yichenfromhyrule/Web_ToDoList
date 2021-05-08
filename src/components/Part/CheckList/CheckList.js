8204eb3d58fc3f1b4a141d40b327ff79f609fc02
yichenfromhyrule <jambako1995@gmail.com>
2021-05-07 22:57:37 -0400

    edit

--- src/components/Part/CheckList/CheckList.js
+++ src/components/Part/CheckList/CheckList.js
@@ -9,13 +9,54 @@ export default class CheckList extends Component {
         this.onChangeDescription = this.onChangeDescription.bind(this);
         this.saveToDo = this.saveToDo.bind(this);
         this.newToDo = this.newToDo.bind(this);
+        this.refreshList = this.refreshList.bind(this);
+        //this.setActiveToDo = this.setActiveTutorial.bind(this);
+        //this.removeAllToDo = this.removeAllTutorials.bind(this);
+        this.onDataChange = this.onDataChange.bind(this);
     
         this.state = {
           title: "",
           description: "",
           published: false,
           submitted: false,
+          todos:[],
+          currentToDo: null,
+          currentIndex: -1,
         };
+    }
+
+    componentDidMount() {
+        ToDoListDataService.getAll().on("value", this.onDataChange);
+    }
+
+    componentWillMount() {
+        ToDoListDataService.getAll().off("value", this.onDataChange);
+    }
+
+    onDataChange(items){
+        let todos = [];
+    
+        items.forEach((item) => {
+          let key = item.key;
+          let data = item.val();
+          todos.push({
+            key: key,
+            title: data.title,
+            description: data.description,
+            published: data.published,
+          });
+        });
+    
+        this.setState({
+          todos: todos,
+        });
+    }
+
+    refreshList() {
+        this.setState({
+          currentToDo: null,
+          currentIndex: -1,
+        });
     }
 
     onChangeToDoTitle(e) {
@@ -58,13 +99,29 @@ export default class CheckList extends Component {
     }
 
     render(){
+        const {todos} = this.state;
         return (
             <div className="checkList_Container">
-                <div className="checkList_Check_Box_Container"></div>
-                <div className="checkList_Button_Container">
+                <div className="checkList_Check_Box_Container">
+                    <div className="list row">
+                        <div className="col-md-6">
+                            <ul className="list-group">
+                                {todos &&
+                                    todos.map((todos, index)=>(
+                                        <li>
+                                            {todos.title}
+                                        </li>
+                                    ))
+                                }
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                
+                <div className="checkList_Editer_Container">
                     {this.state.submitted ? (
                         <div>
-                            <button className="checkList_Edit_Button" onClick={this.newToDo}>
+                            <button className="checkList_Editer_Button" onClick={this.newToDo}>
                                 ADD
                             </button>
                         </div>
